2d688949b6fd3aebb5728759f74cbd612a304a4c
unknown <viktor.staikov@gmail.com>
2016-01-10 11:51:38 +0200

    initial data processing

--- hearthstone-deck-classifier/Classifiers/NaiveBayes.py
+++ hearthstone-deck-classifier/Classifiers/NaiveBayes.py
@@ -2,6 +2,35 @@
     """Naive Bayes Classifier for the purpose of classifying Hearthstone Decks"""
 
     def __init__(self):
-        pass
+        self.total_cards = 0
+        self.raw_data = []
+        self.data = {}
+        self.cards = {}
+
+    def ProcessData(self, raw_data):
+        self.raw_data = raw_data
+        self.total_cards = len(raw_data)
+
+        for card in raw_data:
+            hero_class = card["class"]
+            type = card["archetype"]
+            card_name = card["card-name"]
+
+            if not (card_name in self.cards):
+                self.cards[card_name] = 0
+
+            self.cards[card_name] += 1
 
 
+            if not (hero_class in self.data):
+                self.data[hero_class] = {}
+                self.data[hero_class]["total"] = 0
+            if not (type in self.data[hero_class]):
+                self.data[hero_class][type] = {}
+                self.data[hero_class][type]["total"] = 0
+            if not (card_name in self.data[hero_class][type]):
+                self.data[hero_class][archetype][card_name] = 0
+
+            self.data[hero_class]["total"] += 1
+            self.data[hero_class][type]["total"] += 1
+            self.data[hero_class][archetype][card_name] += 1
